14e69074d8140ca7afd216ad0aff67a0abee628e
CIntissar <intissar.chajaadine@hotmail.com>
2021-09-01 16:37:42 +0200

    animation also

---
+++ Assets/#Project/Scripts/PressButtonDetection.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PressButtonDetection : MonoBehaviour
+{
+    public UnityEvent whenKeyIsPressed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.anyKeyDown)
+        {
+            whenKeyIsPressed?.Invoke();
+        }
+    }
+}
