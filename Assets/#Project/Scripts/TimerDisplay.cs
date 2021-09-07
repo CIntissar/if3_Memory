f0d0ca15cc298415cfef26f6a6bc99a807ab3147
CIntissar <intissar.chajaadine@hotmail.com>
2021-09-07 16:43:42 +0200

    Last push

--- Assets/#Project/Scripts/TimerDisplay.cs
+++ Assets/#Project/Scripts/TimerDisplay.cs
@@ -7,12 +7,24 @@ public class TimerDisplay : MonoBehaviour
 {
     public Text text; 
     private float timer;
+    // private Animator animator;
+
     void Start()
     {
+        // animator = GetComponent<Animator>();
         timer = PlayerPrefs.GetFloat("timeDisplay");
+
+        // if(timer <= 10f)
+        // {
+        //     animator.SetBool("IsRecordTime",true);
+        // }
+        // else
+        // {
+        //     animator.SetBool("IsRecordTime",false);
+        // }
 
         string timeDisplay = timer.ToString("N2"); //format pour les float , pour avoir 2 chiffres derrière la virgule
         text.text = timeDisplay;
-    }
 
+    }
 }
